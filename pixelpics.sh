7205b910a22991ab31486b38b4d66753249d7671
Fiona Burrows <fiona@justfiona.com>
2014-06-26 02:33:39 +0100

    added fix for full screen trackpad speed

--- pixelpics.sh
+++ pixelpics.sh
@@ -3,6 +3,9 @@
 LIBS=./libs
 BIN=./bin/pixelpics
 
-# Run your app
+# Work around for touchpads being too fast in fullscreen mode
+export SDL_MOUSE_RELATIVE=0
+
+# Run pix run
 export LD_LIBRARY_PATH=$LIBS:"$LD_LIBRARY_PATH"
 $BIN $@
